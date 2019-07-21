1f94bad2ef2b1e382ec1a39e116981c9be493146
Costa82 <costa_torito@hotmail.com>
2019-07-21 10:06:28 +0200

    Constantes

--- php/contenido.php
+++ php/contenido.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once './config/constantes.php';
+
 $textos = new Textos();
 
 echo "
@@ -8,8 +10,7 @@ echo "
     <img src='img/logos/TheFantasticL_texto.png' alt='The Fantastic L'/>
 </div> ";
 
-// Buscamos los textos de presentación pasando el valor 'PRES'
-$cod_tipo_texto = 'PRES';
+// Buscamos los textos de presentaciÃ³n pasando el valor 'PRES'
+$cod_tipo_texto = TIPO_TEXTO_PRESENTACION;
 
 $textos->mostrarTexto($cod_tipo_texto);
-
--- php/contenido_blog.php
+++ php/contenido_blog.php
@@ -1,10 +1,12 @@
 <?php
 
+require_once '../config/constantes.php';
+
 $textos = new Textos();
 
 echo "<h1>BLOG</h1>";
 
 // Buscamos los textos del blog pasando el valor 'BLOG'
-$cod_tipo_texto = 'BLOG';
+$cod_tipo_texto = TIPO_TEXTO_BLOG;
 
 $textos->mostrarTexto($cod_tipo_texto);
